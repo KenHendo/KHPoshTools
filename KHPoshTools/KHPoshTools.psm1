03a516601f8aade0be4f108f9bfcdd4ef672e485
Ken Henderson <ken@essentialinfrastructure.co.uk>
2018-10-24 23:05:11 +0100

    added first basic function to module

--- KHPoshTools/KHPoshTools.psm1
+++ KHPoshTools/KHPoshTools.psm1
@@ -1,8 +1,42 @@
 Function Get-KHComputer {
 
+    [CmdletBinding()]
 
-}
+    Param(
+
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            Position = 0)]
+        [string[]]$ComputerName
+    )
+
+    Begin {}
+    
+    Process {
+        Foreach ($Computer in $ComputerName) {
+        
+            #Create CIMSession for the various queries
+            $Param = @{
+                ComputerName = $Computer
+            }
+
+            $Session = New-CimSession @Param
+
+            #Query the computer
+            $CompSysParam = @{
+                CimSession   = $Session
+                Classname = 'Win32_ComputerSystem'
+            }
+
+            $CompSysInfo = Get-CimInstance @CompSysParam
+            Write-Output $CompSysInfo
+        }
+    }
+
+    End {}
+}#Function Get-KHComputer
 
 Function Set-KHComputer {
-    
+
 }
--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -6,10 +6,17 @@ Param(
 If (-not $Scriptpath) {
 
     $ModuleName = 'KHPoshTools'
+    
+    If ((Get-module).name -contains $ModuleName) {
+        Write-Host "$Modulename already found in runspace.  Unloading for a clean start"
+        Remove-Module -Name $ModuleName -Force
+    }
+   
     $here = Split-Path -Parent $MyInvocation.MyCommand.Path 
     $ModulePath = (Split-Path -parent $here) + "\$ModuleName"
     $sut = $modulepath + "\$ModuleName.psm1"
-    . $sut
+    #. $sut
+    import-module $ModulePath -Force 
 }
 
 Else {
@@ -19,52 +26,66 @@ Else {
 
 
 Describe "KHPoshTools : UNIT TESTS" {
+
+    $CompNameTestCases = @(
+
+        @{ComputerName = 'TestMachine1'; ExpectedCount = 1; TestName = 'Single Computer'}
+        @{ComputerName = @('TestMachine1', 'TestMachine2', 'TestMachine3'); ExpectedCount = 3; TestName = 'Multiple Computers'}
+    )
+
+    Mock New-CimSession -ModuleName 'KHPoshTools' {
+        New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession'
+    }
+
+    #Mock Get-CimInstance {} -ParameterFilter {! $PSBoundParameters.ContainsKey('ComputerName')}
+    Mock Get-CimInstance -ModuleName 'KHPoshTools' { 1 }
     
     Write-Verbose 'Testing that the function accepts input correctly'
     Context Input {
-
-        Mock New-CimSession {
-            $MockSession = New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession'
-            Write-Output $MockSession
-        }
-        #Mock Get-CimInstance {} -ParameterFilter {! $PSBoundParameters.ContainsKey('ComputerName')}
-        Mock Get-CimInstance { 1 }
-
-        $CompNameTestCases = @(
-
-            @{ComputerName = 'TestMachine1'; ExpectedCount = 1; TestName = 'Single Computer'}
-            @{ComputerName = @('TestMachine1', 'TestMachine2', 'TestMachine3'); ExpectedCount = 3; TestName = 'Multiple Computers'}
-        )
-
-        It 'Creates a single CIMSesssion for each computername supplied' {
-
-        }
         
         It 'Returns the right number of objects based on ComputerName(s) entered: <Testname>' -TestCases $CompNameTestCases {
             Param(
                 $ComputerName,
                 $ExpectedCount
             )
-            Mock New-CimSession {New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession' }
             $result = Get-KHComputer -ComputerName $ComputerName
             $result.count | should BeExactly $ExpectedCount  
         } 
 
-        It 'Does not Accept an empty or null value for computername' -skip {
-
+        It 'Does not Accept a Null value for computername' {
+            { Get-KHComputer -ComputerName $null} | should throw
         }
 
-        It "Accepts input by Pipeline" -skip {
-
+        It 'Accepts input by Pipeline: <Testname>' -TestCases $CompNameTestCases {
+            Param (
+                $Computername,
+                $ExpectedCount
+            )
+            $result = $ComputerName | Get-KHComputer
+            $result.count | should BeExactly $ExpectedCount  
         }
         
-        It "Accepts a ComputerName via positional parameter" -skip {
-
+        It "Accepts a ComputerName via positional parameter" {
+            {Get-KHComputer 'TestComputer'} | should be $true
         }
     }#context Input
 
     Write-Verbose 'Testing that the function executes its internal code to produce the desired result'
     Context Execution {
+        
+        Mock New-CimSession -ModuleName 'KHPoshTools' {
+            New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession'
+        }
+        It 'Creates a single CIMSesssion for each computername supplied: <Testname>' -TestCases $CompNameTestCases {
+            Param (
+                $Computername,
+                $ExpectedCount
+            )
+            Get-KHComputer -ComputerName $Computername
+            Assert-MockCalled -CommandName 'New-CimSession' -Times $ExpectedCount
+        }
+
+        It 'Should remove the CIMSession when done' -skip {}
 
     }#Context Execution
 
